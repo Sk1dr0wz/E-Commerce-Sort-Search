9aaebe663a3a16642da2f600b331d114addd4a34
dzz9 <87163946+dzz9@users.noreply.github.com>
2024-11-21 01:40:44 +0800

    Updated test input for complete sort_merge

--- !automate/Sort-Merge.py
+++ !automate/Sort-Merge.py
@@ -2,5 +2,5 @@ from subprocess import Popen, PIPE
 import os
 
 p = Popen(['python', 'main.py'], cwd=os.path.dirname(os.getcwd()), stdin=PIPE, stdout=PIPE, shell=False)
-out = p.communicate(input=b'2\n2\n4\n4\n4\n', timeout=5)
+out = p.communicate(input=b'2\n2\n1\n2\n3\n4\n4\n4\n', timeout=5)
 print(str(out[0]).replace('\\r\\n', '\n'))
